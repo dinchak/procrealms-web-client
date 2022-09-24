f216a2c18e77b6f3bf0cee1e2507f88dc178c56b
Tom <dinchak@gmail.com>
2022-09-23 19:49:54 -0500

    battle style tweaks

--- src/components/BattleStatus.vue
+++ src/components/BattleStatus.vue
@@ -2,13 +2,20 @@
   <div class="battle-status" ref="battleStatus">
     <div class="side good">
       <div class="entity" v-for="entity in getSide('good')" :key="entity.name">
-        <div class="vitals">
-          <n-progress type="circle" status="success" :percentage="entity.hp">HP</n-progress>
-          <n-progress type="circle" status="default" :percentage="entity.en">EN</n-progress>
-          <n-progress type="circle" status="warning" :percentage="entity.st">ST</n-progress>
-          <n-progress type="circle" status="error" :percentage="entity.nextAction / 20 * 100">AC</n-progress>
+        <div class="action">
+          <n-progress type="circle" status="error" :percentage="entity.nextAction / 20 * 100" :show-indicator="false"></n-progress>
         </div>
-        <div class="name" v-html="ansiSpan(entity.name)"></div>
+        <div class="vitals" v-if="entity.hp > 0">
+          <n-progress type="line" status="success" :percentage="entity.hp" :show-indicator="false"></n-progress>
+          <n-progress type="line" status="default" :percentage="entity.en" :show-indicator="false"></n-progress>
+          <n-progress type="line" status="warning" :percentage="entity.st" :show-indicator="false"></n-progress>
+        </div>
+        <div class="vitals bold-red" v-if="entity.hp == 0">DEAD</div>
+        <div class="name-container">
+          <div class="name" v-html="ansiSpan(entity.name)" @click="target(entity)"></div>
+          <div v-if="entity.target" class="targeting" v-html="'Target: ' + ansiSpan(entity.target)"></div>
+          <div v-if="!entity.target" class="targeting">No target</div>
+        </div>
         <div class="caret" v-if="entity.isActing">&lt;</div>
         <div class="caret" v-if="!entity.isActing"></div>
       </div>
@@ -20,12 +27,19 @@
       <div class="entity" v-for="entity in getSide('evil')" :key="entity.name">
         <div class="caret" v-if="entity.isActing">&gt;</div>
         <div class="caret" v-if="!entity.isActing"></div>
-        <div class="name" v-html="ansiSpan(entity.name)" @click="target(entity)"></div>
-        <div class="vitals">
-          <n-progress type="circle" status="success" :percentage="entity.hp">HP</n-progress>
-          <n-progress type="circle" status="default" :percentage="entity.en">EN</n-progress>
-          <n-progress type="circle" status="warning" :percentage="entity.st">ST</n-progress>
-          <n-progress type="circle" status="error" :percentage="entity.nextAction / 20 * 100">AC</n-progress>
+        <div class="name-container">
+          <div class="name" v-html="ansiSpan(entity.name)" @click="target(entity)"></div>
+          <div v-if="entity.target" class="targeting" v-html="'Target: ' + ansiSpan(entity.target)"></div>
+          <div v-if="!entity.target" class="targeting">No target</div>
+        </div>
+        <div class="vitals" v-if="entity.hp > 0">
+          <n-progress type="line" status="success" :percentage="entity.hp" :show-indicator="false"></n-progress>
+          <n-progress type="line" status="default" :percentage="entity.en" :show-indicator="false"></n-progress>
+          <n-progress type="line" status="warning" :percentage="entity.st" :show-indicator="false"></n-progress>
+        </div>
+        <div class="vitals bold-red" v-if="entity.hp == 0">DEAD</div>
+        <div class="action">
+          <n-progress type="circle" status="error" :percentage="entity.nextAction / 20 * 100" :show-indicator="false"></n-progress>
         </div>
       </div>
     </div>
@@ -34,13 +48,20 @@
   <div class="mobile-battle-status">
     <div class="side good">
       <div class="entity" v-for="entity in getSide('good')" :key="entity.name">
-        <div class="vitals">
-          <n-progress type="circle" status="success" :percentage="entity.hp">HP</n-progress>
-          <n-progress type="circle" status="default" :percentage="entity.en">EN</n-progress>
-          <n-progress type="circle" status="warning" :percentage="entity.st">ST</n-progress>
-          <n-progress type="circle" status="error" :percentage="entity.nextAction / 20 * 100">AC</n-progress>
+        <div class="action">
+          <n-progress type="circle" status="error" :percentage="entity.nextAction / 20 * 100" :show-indicator="false"></n-progress>
         </div>
-        <div class="name" v-html="ansiSpan(entity.name)"></div>
+        <div class="vitals" v-if="entity.hp > 0">
+          <n-progress type="line" status="success" :percentage="entity.hp" :show-indicator="false"></n-progress>
+          <n-progress type="line" status="default" :percentage="entity.en" :show-indicator="false"></n-progress>
+          <n-progress type="line" status="warning" :percentage="entity.st" :show-indicator="false"></n-progress>
+        </div>
+        <div class="vitals bold-red" v-if="entity.hp == 0">DEAD</div>
+        <div class="name-container">
+          <div class="name" v-html="ansiSpan(entity.name)" @click="target(entity)"></div>
+          <div v-if="entity.target" class="targeting" v-html="'Target: ' + ansiSpan(entity.target)"></div>
+          <div v-if="!entity.target" class="targeting">No target</div>
+        </div>
         <div class="caret" v-if="entity.isActing">&lt;</div>
         <div class="caret" v-if="!entity.isActing"></div>
       </div>
@@ -52,12 +73,19 @@
       <div class="entity" v-for="entity in getSide('evil')" :key="entity.name">
         <div class="caret" v-if="entity.isActing">&gt;</div>
         <div class="caret" v-if="!entity.isActing"></div>
-        <div class="name" v-html="ansiSpan(entity.name)" @click="target(entity)"></div>
-        <div class="vitals">
-          <n-progress type="circle" status="success" :percentage="entity.hp">HP</n-progress>
-          <n-progress type="circle" status="default" :percentage="entity.en">EN</n-progress>
-          <n-progress type="circle" status="warning" :percentage="entity.st">ST</n-progress>
-          <n-progress type="circle" status="error" :percentage="entity.nextAction / 20 * 100">AC</n-progress>
+        <div class="name-container">
+          <div class="name" v-html="ansiSpan(entity.name)" @click="target(entity)"></div>
+          <div v-if="entity.target" class="targeting" v-html="'Target: ' + ansiSpan(entity.target)"></div>
+          <div v-if="!entity.target" class="targeting">No target</div>
+        </div>
+        <div class="vitals" v-if="entity.hp > 0">
+          <n-progress type="line" status="success" :percentage="entity.hp" :show-indicator="false"></n-progress>
+          <n-progress type="line" status="default" :percentage="entity.en" :show-indicator="false"></n-progress>
+          <n-progress type="line" status="warning" :percentage="entity.st" :show-indicator="false"></n-progress>
+        </div>
+        <div class="vitals bold-red" v-if="entity.hp == 0">DEAD</div>
+        <div class="action">
+          <n-progress type="circle" status="error" :percentage="entity.nextAction / 20 * 100" :show-indicator="false"></n-progress>
         </div>
       </div>
     </div>
@@ -109,7 +137,8 @@ onMounted(() => {
   align-items: center;
 
   .vs {
-    font-size: 12px;
+    font-size: 14px;
+    line-height: 40px;
     align-self: center;
     text-align: center;
     color: darken(#c50f1f, 5%);
@@ -119,13 +148,20 @@ onMounted(() => {
     width: 100%;
     .entity {
       width: 100%;
-      padding: 2px 2px 5px 2px;
-      border-bottom: 1px solid rgb(30, 0, 0);
+      padding: 2px;
       align-items: center;
       &:last-child {
         border: 0;
         margin-bottom: 0;
         padding: 2px;
+      }
+      .action {
+        display: flex;
+        justify-content: center;
+        .n-progress {
+          height: 20px;
+          width: 20px;
+        }
       }
       .vitals {
         width: 130px;
@@ -135,15 +171,24 @@ onMounted(() => {
         flex-wrap: wrap;
         justify-content: space-around;
         align-items: center;
+        font-size: 16px;
       }
-      .name {
+      .name-container {
         width: ~"calc(100% - 10px)";
         margin: 0 5px;
-        font-size: 20px;
-        line-height: 14px;
-        cursor: pointer;
-        &:hover {
-          text-decoration: underline;
+        display: flex;
+        flex-direction: column;
+        .name {
+          font-size: 20px;
+          line-height: 16px;
+          cursor: pointer;
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+        .targeting {
+          font-size: 14px;
+          line-height: 10px;
         }
       }
       .caret {
@@ -152,25 +197,36 @@ onMounted(() => {
         color: #f9f1a5;
       }
       .n-progress {
-        width: 28px;
-        height: 28px;
-        margin: 0 2px;
+        // width: 28px;
+        // height: 32px;
+        // margin: 0 2px;
+        --n-rail-height: 8px !important;
+        .n-progress-content {
+          .n-progress-graph {
+            .n-progress-graph-line-rail {
+              border-radius: 0 !important;
+              .n-progress-graph-line-fill {
+                border-radius: 0 !important;
+              }
+            }
+          }
+        }
       }
     }
 
     &.good {
       .entity {
         display: grid;
-        grid-template-columns: 130px calc(100% - 150px) 10px;
+        grid-template-columns: 35px 130px calc(100% - 180px) 10px;
       }
     }
 
     &.evil {
       .entity {
         display: grid;
-        grid-template-columns: 10px calc(100% - 160px) 130px;
+        grid-template-columns: 10px calc(100% - 180px) 130px 35px;
         margin-right: 10px;
-        .name {
+        .name-container {
           text-align: right;
         }
       }
@@ -211,6 +267,14 @@ onMounted(() => {
         margin-bottom: 0;
         padding: 2px;
       }
+      .action {
+        display: flex;
+        justify-content: center;
+        .n-progress {
+          height: 20px;
+          width: 20px;
+        }
+      }
       .vitals {
         width: 130px;
         align-content: center;
@@ -219,15 +283,24 @@ onMounted(() => {
         flex-wrap: wrap;
         justify-content: space-around;
         align-items: center;
+        font-size: 16px;
       }
-      .name {
+      .name-container {
         width: ~"calc(100% - 10px)";
         margin: 0 5px;
-        font-size: 20px;
-        line-height: 20px;
-        cursor: pointer;
-        &:hover {
-          text-decoration: underline;
+        display: flex;
+        flex-direction: column;
+        .name {
+          font-size: 20px;
+          line-height: 16px;
+          cursor: pointer;
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+        .targeting {
+          font-size: 14px;
+          line-height: 10px;
         }
       }
       .caret {
@@ -236,25 +309,36 @@ onMounted(() => {
         color: #f9f1a5;
       }
       .n-progress {
-        width: 28px;
-        height: 32px;
-        margin: 0 2px;
+        // width: 28px;
+        // height: 32px;
+        // margin: 0 2px;
+        --n-rail-height: 8px !important;
+        .n-progress-content {
+          .n-progress-graph {
+            .n-progress-graph-line-rail {
+              border-radius: 0 !important;
+              .n-progress-graph-line-fill {
+                border-radius: 0 !important;
+              }
+            }
+          }
+        }
       }
     }
 
     &.good {
       .entity {
         display: grid;
-        grid-template-columns: 130px calc(100% - 150px) 10px;
+        grid-template-columns: 35px 130px calc(100% - 180px) 10px;
       }
     }
 
     &.evil {
       .entity {
         display: grid;
-        grid-template-columns: 10px calc(100% - 160px) 130px;
+        grid-template-columns: 10px calc(100% - 180px) 130px 35px;
         margin-right: 10px;
-        .name {
+        .name-container {
           text-align: right;
         }
       }
@@ -267,18 +351,18 @@ onMounted(() => {
     .side {
       .entity {
         .vitals {
-          width: 75px;
+          width: 60px;
         }
       }
       &.good {
         .entity {
           display: grid;
-          grid-template-columns: 75px calc(100% - 100px) 10px;
+          grid-template-columns: 35px 60px calc(100% - 105px) 10px;
         }
       }
       &.evil {
         .entity {
-          grid-template-columns: 10px calc(100% - 100px) 75px;
+          grid-template-columns: 10px calc(100% - 105px) 60px 35px;
         }
       }
     }
@@ -299,18 +383,18 @@ onMounted(() => {
     .side {
       .entity {
         .vitals {
-          width: 75px;
+          width: 60px;
         }
       }
       &.good {
         .entity {
           display: grid;
-          grid-template-columns: 75px calc(100% - 100px) 10px;
+          grid-template-columns: 35px 60px calc(100% - 105px) 10px;
         }
       }
       &.evil {
         .entity {
-          grid-template-columns: 10px calc(100% - 100px) 75px;
+          grid-template-columns: 10px calc(100% - 105px) 60px 35px;
         }
       }
     }
@@ -319,15 +403,22 @@ onMounted(() => {
 
 @media screen and (max-height: 500px) {
   .mobile-battle-status {
+    .vs {
+      line-height: 20px;
+    }
     .side {
       .entity {
-        .name {
-          font-size: 16px;
-          line-height: 12px;
-        }
         .n-progress {
-          width: 24px;
-          height: 24px;
+          // width: 28px;
+          // height: 32px;
+          // margin: 0 2px;
+          --n-rail-height: 7px !important;
+        }
+        .name-container {
+          .name {
+            font-size: 16px;
+            line-height: 12px;
+          }
         }
       }
     }
