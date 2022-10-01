c4acd5492326d32d796c5f134bbe53ac9bae9c06
Tom <dinchak@gmail.com>
2022-10-01 07:56:32 -0500

    add show/hide tab option

--- src/components/OptionsMenu.vue
+++ src/components/OptionsMenu.vue
@@ -46,6 +46,15 @@
       </template>
     </n-switch>
 
+    <n-switch v-model:value="state.options.showTabs">
+      <template #checked>
+        Show Tabs
+      </template>
+      <template #unchecked>
+        Hide Tabs
+      </template>
+    </n-switch>
+
     <n-button type="success" @click="goFullscreen()" ghost>Full Screen</n-button>
     <n-button type="warning" @click="state.showHelp = !state.showHelp" ghost>Help</n-button>
     <n-button type="error" @click="state.showLogout = true" ghost>Logout</n-button>
