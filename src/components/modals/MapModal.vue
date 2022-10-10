c3e02f7f8b3dff5df62067bd47d9cd570a02eeca
robb <contact@robertsoldan.com>
2022-10-10 20:16:00 +0100

    Map modal

---
+++ src/components/modals/MapModal.vue
@@ -0,0 +1,74 @@
+<template>
+  <n-card :class="getSideClass()" v-if="state.modals.mapModal">
+    <p class="close" v-on:click="closeModal()">x</p>
+    <div v-for="(line, id) in largeMap" :key="id">
+      <div v-html="ansiSpan(line)"></div>
+    </div>
+  </n-card>
+</template>
+
+<script setup>
+import { NCard } from 'naive-ui'
+import { ref, watch } from 'vue'
+import { ansiSpan } from 'ansi-to-span'
+import { state } from '@/composables/state'
+import { command_ids } from '@/composables/constants/command_ids'
+import { useWebSocket } from '@/composables/web_socket'
+
+const { cmd } = useWebSocket()
+const MAP_ID = command_ids.MAP
+const largeMap = ref([])
+
+watch(() => state.commandCache[MAP_ID], () => {
+  if (state.modals.mapModal) {
+    largeMap.value = state.commandCache[MAP_ID].replaceAll(' ',  '&nbsp;').split(' ')
+  }
+})
+
+watch(() => state.gameState.map, () => {
+  cmd('map', MAP_ID)
+})
+
+function getSideClass() {
+  return state.options.swapControls ? 'right' : 'left'
+}
+
+function closeModal() {
+  state.modals.mapModal = false
+}
+
+cmd('map', MAP_ID)
+</script>
+
+<style scoped lang="less">
+.left {
+  left: 5px;
+}
+
+.right {
+  right: 5px;
+}
+
+.n-card {
+  position: absolute;
+  max-width: 62vw;
+  margin-top: 35px;
+  z-index: 2;
+  line-height: 10px;
+}
+
+.close {
+  position: absolute;
+  top: -10px;
+  right: 10px;
+  cursor: pointer;
+  font-size: 1.4em;
+}
+
+@media screen and (max-width: 600px) {
+  .n-card {
+    font-size: 15px;
+    line-height: 14px;
+  }
+}
+</style>
