4219b2d0c10d80c5be7f1605eef7f8646cc0dac3
Tom <dinchak@gmail.com>
2022-10-17 21:21:59 -0500

    refactored battle display

---
+++ src/components/main-area/battle-items/BattleCaret.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="caret" v-if="entity.isActing">{{ caret }}</div>
+  <div class="caret" v-if="!entity.isActing"></div>
+</template>
+<script setup>
+import { defineProps, toRefs } from 'vue'
+
+const props = defineProps({
+  entity: Object,
+  caret: String
+})
+const { entity } = toRefs(props)
+</script>
+<style lang="less">
+</style>
---
+++ src/components/main-area/battle-items/BattleName.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="name-container" @click="target(entity)">
+    <div class="name" v-html="ansiToHtml(entity.name)"></div>
+    <div class="affects" v-html="getAffects(entity)"></div>
+    <div v-if="entity.targetName" class="targeting" v-html="'Target: ' + ansiToHtml(entity.targetName)"></div>
+    <div v-if="!entity.targetName" class="targeting">No target</div>
+  </div>
+</template>
+<script setup>
+import { defineProps, toRefs } from 'vue'
+import stripAnsi from 'strip-ansi'
+
+import { helpers } from '@/composables/helpers'
+import { useWebSocket } from '@/composables/web_socket'
+
+const { ansiToHtml } = helpers()
+const { cmd } = useWebSocket()
+
+const props = defineProps({
+  entity: Object,
+})
+const { entity } = toRefs(props)
+
+function target (entity) {
+  cmd(`target ${stripAnsi(entity.tag)}`)
+}
+
+function getAffects (entity) {
+  if (!entity.affects.length) {
+    return 'No affects'
+  }
+  return ansiToHtml(entity.affects.join(' '))
+}
+</script>
+<style lang="less">
+</style>
