706cd27650b9a842a8d808f2cdeede1788d3d3a6
jeromechoo <hello@jeromechoo.com>
2023-08-07 13:35:13 -0500

    Add togglable map sizes

--- src/components/modals/MapModal.vue
+++ src/components/modals/MapModal.vue
@@ -1,18 +1,23 @@
 <template>
-  <n-card :class="getSideClass()" v-if="state.modals.mapModal">
-    <p class="close" v-on:click="closeModal()">x</p>
+  <n-card title="Map" size="small" :class="[getSideClass(), geMapModalSizeClass()]" v-if="state.modals.mapModal" content-style="display: flex; flex-wrap: wrap; justify-content: center; align-content:center; overflow:hidden;" closable @close="closeModal()">
+    <template #header-extra>
+      <n-button text @click="toggleMapModalSize()">
+        <n-icon size="15"><WindowOutlined></WindowOutlined></n-icon>
+      </n-button>
+    </template>
     <div class="ansi" v-for="(line, id) in largeMap" :key="id" v-html-safe="ansiToHtml(line)"></div>
   </n-card>
 </template>
 
 <script setup>
-import { NCard } from 'naive-ui'
+import { NCard, NButton, NIcon } from 'naive-ui'
 import { ref, watch, onMounted } from 'vue'
 import { helpers } from '@/composables/helpers'
 import { state } from '@/composables/state'
 import { command_ids } from '@/composables/constants/command_ids'
 import { useWebSocket } from '@/composables/web_socket'
 import { useKeyHandler } from '@/composables/key_handler'
+import WindowOutlined from '@vicons/material/WindowOutlined'
 
 const { cmd } = useWebSocket()
 const { onKeydown, keyState } = useKeyHandler()
@@ -42,6 +47,22 @@ watch(() => state.gameState.map, () => {
     cmd('map', MAP_ID)
   }
 })
+
+function toggleMapModalSize() {
+  if (state.modals.mapModalSize == "small") {
+    state.modals.mapModalSize = "medium"
+  }
+  else if (state.modals.mapModalSize == "medium") {
+    state.modals.mapModalSize = "large"
+  }
+  else {
+    state.modals.mapModalSize = "small"
+  }
+}
+
+function geMapModalSizeClass() {
+  return "map-" + state.modals.mapModalSize
+}
 
 function getSideClass() {
   return state.options.swapControls ? 'map-modal right' : 'map-modal left'
@@ -77,23 +98,33 @@ onMounted(() => {
 .ansi {
   font-family: 'Inconsolata', monospace;
   white-space: pre;
-  font-size: 13px;
+  font-size: 16px;
   line-height: 13px;
 }
 
 .left {
-  left: 5px;
+  right: 10px;
 }
 
 .right {
-  right: 5px;
+  left: 10px;
 }
 
 .n-card {
   position: absolute;
   margin-top: 35px;
-  max-width: calc(100vw - 310px);
+  max-width: calc(100vw - 290px);
   z-index: 2;
+}
+
+.n-card.map-small {
+  width: 400px;
+  height: 400px;
+}
+
+.n-card.map-medium {
+  width: 600px;
+  height: 600px;
 }
 
 .close {
