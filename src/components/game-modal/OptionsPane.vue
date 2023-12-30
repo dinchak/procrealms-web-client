320e1ff8fc79d1081fe25bb7d4a4af0bb9660889
tom <dinchak@gmail.com>
2023-12-30 09:26:25 -0500

    added options pane skeleton

---
+++ src/components/game-modal/OptionsPane.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="scroll-container">
+    <NGrid class="options" cols="1 800:2">
+      <NGi>Options</NGi>
+    </NGrid>
+  </div>
+</template>
+
+<script setup>
+// import { ref } from 'vue'
+// import { NGrid, NGi, NProgress } from 'naive-ui'
+// import { state } from '@/composables/state'
+// import { useHelpers } from '@/composables/helpers'
+
+// const { ansiToHtml } = useHelpers()
+
+</script>
+<style lang="less" scoped>
+.scroll-container {
+  height: calc(100vh - 225px);
+  overflow-y: scroll;
+}
+</style>
