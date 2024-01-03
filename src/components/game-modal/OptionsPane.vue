1fa655d2dc2c6cc3ae169e9a2c9b799066cf7832
tom <dinchak@gmail.com>
2024-01-02 21:08:21 -0500

    add options pane

--- src/components/game-modal/OptionsPane.vue
+++ src/components/game-modal/OptionsPane.vue
@@ -1,23 +1,323 @@
 <template>
   <div class="scroll-container">
-    <NGrid class="options" cols="1 800:2">
-      <NGi>Options</NGi>
-    </NGrid>
+    <div class="row">
+      <NGrid class="options" cols="1">
+        <NGi>
+          <h3>HUD</h3>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-hud-command-controls">Command Controls</label>
+            <NSwitch id="option-hud-command-controls" v-model:value="state.options.hudCommandControls" aria-label="Command Controls" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-hud-movement-controls">Movement Controls</label>
+            <NSwitch id="option-hud-movement-controls" v-model:value="state.options.hudMovementControls" aria-label="Movement Controls" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-show-quick-slots">Show Quick Slots</label>
+            <NSwitch id="option-show-quick-slots" v-model:value="state.options.showQuickSlots" aria-label="Show Quick Slots" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-overlay-controls">Overlay Controls</label>
+            <NSwitch id="option-overlay-controls" v-model:value="state.options.overlayControls" aria-label="Overlay Controls" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-overlay-minimap">Overlay Minimap</label>
+            <NSwitch id="option-overlay-minimap" v-model:value="state.options.showOverlayMinimap" aria-label="Overlay Minimap" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+      </NGrid>
+
+      <NGrid class="options" cols="1">
+        <NGi>
+          <h3>Sidebar</h3>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-show-map-area">Show Map Area</label>
+            <NSwitch id="option-show-map-area" v-model:value="state.options.showMapArea" aria-label="Show Map Area" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-fixed-minimap">Fixed Minimap</label>
+            <NSwitch id="option-fixed-minimap" v-model:value="state.options.fixedMap" aria-label="Fixed Mini Map" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-swap-menu-side">Swap Menu Side</label>
+            <NSwitch id="option-swap-menu-side" v-model:value="state.options.swapControls" aria-label="Swap Menu Side" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+      </NGrid>
+
+      <NGrid class="options" cols="1">
+        <NGi>
+          <h3>General</h3>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-chat-in-main-output">Chat In Main Output</label>
+            <NSwitch id="option-chat-in-main-output" v-model:value="state.options.chatInMain" aria-label="Chat In Main Output" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-keep-sent-commands">Keep Sent Commands</label>
+            <NSwitch id="option-keep-sent-commands" v-model:value="state.options.keepSentCommands" aria-label="Keep Sent Commands" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-numpad-movement">Numpad Movement</label>
+            <NSwitch id="option-movement-option" v-model:value="state.options.numPadMovement" aria-label="Numpad Movement" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+        <NGi>
+          <div class="option">
+            <label for="option-wasd-movement">WASD Movement</label>
+            <NSwitch id="option-wasd-movement" v-model:value="state.options.wasdMovement" aria-label="WASD Movement" class="selectable"></NSwitch>
+          </div>
+        </NGi>
+
+      </NGrid>
+    </div>
+
+    <div class="row">
+      <NGrid class="options" cols="1">
+        <NGi>
+          <h3 class="pad-top">Fonts</h3>
+        </NGi>
+
+        <NGi style="text-align: center; margin-bottom: 20px;">
+          <NSelect
+            class="font-selector selectable"
+            v-model:value="state.options.fontFamily"
+            placeholder="Select Font"
+            :options="fontOptions"
+            aria-label="Select Font"
+            @update:value="setFont"
+          />
+        </NGi>
+        <NGi style="text-align: center">
+          <NRadioGroup v-model:value="selectedFontSize" name="radiobuttongroup1" class="font-size-selector">
+            <NRadioButton
+              v-for="fontSize in fontSizes"
+              :key="fontSize.value"
+              :value="fontSize.value"
+              :label="fontSize.label"
+              @change="changeFontSize"
+              class="selectable"
+            />
+          </NRadioGroup>
+        </NGi>
+      </NGrid>
+
+      <NGrid class="options" cols="1">
+        <NGi>
+          <h3>Gamepad</h3>
+        </NGi>
+        <NGi>
+          <ul v-bind:key="idx" v-for="(gamepad, idx) in state.gamepads">
+            <li>{{ gamepad }}</li>
+          </ul>
+        </NGi>
+        <NGi>
+          <div style="text-align: center" v-if="Object.values(state.gamepads).length === 0">No gamepads connected.</div>
+        </NGi>
+      </NGrid>
+
+      <NGrid class="options" cols="1">
+        <NGi style="text-align: center">
+          <NButton style="margin-top: 30px" class="menu-button selectable" type="info" @click="openTriggersModal()" ghost>Triggers</NButton>
+        </NGi>
+        <NGi style="text-align: center">
+          <NButton class="menu-button selectable" type="success" @click="goFullscreen()" ghost>Full Screen</NButton>
+        </NGi>
+        <NGi style="text-align: center">
+          <NButton class="menu-button selectable" type="warning" @click="openHelpModal()" ghost>Help</NButton>
+        </NGi>
+        <NGi style="text-align: center">
+          <NButton class="menu-button selectable" type="error" @click="openLogoutModal()" ghost>Logout</NButton>
+        </NGi>
+      </NGrid>
+    </div>
+
   </div>
 </template>
 
 <script setup>
-// import { ref } from 'vue'
-// import { NGrid, NGi, NProgress } from 'naive-ui'
-// import { state } from '@/composables/state'
-// import { useHelpers } from '@/composables/helpers'
+import { watch, ref } from 'vue'
+import { NGrid, NGi, NSwitch, NRadioGroup, NRadioButton, NSelect, NButton } from 'naive-ui'
+import { state, setMode } from '@/composables/state'
+import { useWindowHandler } from '@/composables/window_handler'
 
-// const { ansiToHtml } = useHelpers()
+const { triggerResize } = useWindowHandler()
 
+const fontSizes = [{
+  value: '14px',
+  label: 'Small'
+}, {
+  value: '16px',
+  label: 'Medium'
+}, {
+  value: '18px',
+  label: 'Large'
+}]
+
+const selectedFontSize = ref(state.options.fontSize)
+
+const fontOptions = [{
+  label: 'Consola Mono',
+  value: 'Consola Mono, monospace'
+}, {
+  label: 'DOS',
+  value: 'DOS, monospace'
+}, {
+  label: 'F25 Bank Printer',
+  value: 'F25 Bank Printer, monospace'
+}, {
+  label: 'Inconsolata',
+  value: 'Inconsolata, monospace'
+}, {
+  label: 'Monofonto',
+  value: 'Monofonto, monospace'
+}, {
+  label: 'Source Code Pro',
+  value: 'Source Code Pro, monospace'
+}, {
+  label: 'Ubuntu Mono',
+  value: 'Ubuntu Mono, monospace'
+}]
+
+watch(state.options, () => localStorage.setItem('options', JSON.stringify(state.options)))
+
+async function goFullscreen () {
+  let app = document.getElementById('app')
+  await app.requestFullscreen()
+  triggerResize()
+}
+
+function setFont () {
+  document.getElementsByTagName('body')[0].style.fontFamily = state.options.fontFamily
+  triggerResize()
+}
+
+function changeFontSize () {
+  state.options.fontSize = selectedFontSize.value
+  document.getElementsByTagName('html')[0].style.fontSize = selectedFontSize.value
+  triggerResize()
+}
+
+function openTriggersModal () {
+  setMode('modal')
+  state.modals.triggersModal = true
+}
+
+function openHelpModal () {
+  setMode('modal')
+  state.modals.helpModal = true
+}
+
+function openLogoutModal () {
+  setMode('modal')
+  state.modals.logoutModal = true
+}
 </script>
 <style lang="less" scoped>
 .scroll-container {
   height: calc(100vh - 225px);
   overflow-y: scroll;
+  max-width: 1200px;
+  margin: 0 auto;
+
+  .row {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    justify-content: center;
+    .options {
+      display: flex;
+      flex-direction: column;
+      flex-basis: 300px;
+      h3 {
+        text-align: center;
+      }
+      .option {
+        max-width: 250px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
+
+        &.selected {
+          background: #121;
+        }
+
+        label {
+          display: block;
+          width: 100%;
+          text-align: right;
+          margin-right: 20px;
+        }
+
+        .n-switch {
+          &.selected {
+            box-shadow: 0 0 5px #f8ff25;
+            color: #f8ff25;
+            background-color: #ff0;
+          }
+        }
+      }
+
+      .n-button, .n-select {
+        &.selected {
+          box-shadow: 0 0 5px #f8ff25;
+          color: #f8ff25;            
+        }
+      }
+
+      .n-radio-button {
+        &.selected {
+          box-shadow: 0 0 5px #f8ff25;
+          color: #f8ff25;
+          // background-color: #ff0;
+        }
+      }
+
+      .menu-button {
+        margin-bottom: 10px;
+        width: 200px;
+        &:first-child {
+          margin-top: 10px;
+        }
+      }
+
+    }
+  }
 }
 </style>
