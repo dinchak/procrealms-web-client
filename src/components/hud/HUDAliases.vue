2a3284eb0bafb3344f4b3fa73c1f62f768df849a
tom <dinchak@gmail.com>
2024-02-24 19:56:58 -0500

    add alias interface

---
+++ src/components/hud/HUDAliases.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="aliases" v-if="state.options.showAliases" :style="{ bottom: getBottom() }">
+    <div v-if="getAliases().length == 0">
+      No aliases defined
+    </div>
+    <div v-for="alias in getAliases()" :key="alias.alias">
+      <n-button size="small" @click="runAlias(alias.alias)">{{ alias.alias }}</n-button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { state, showHUD } from '@/composables/state'
+import { NButton } from 'naive-ui'
+import { useWebSocket } from '@/composables/web_socket'
+
+const { cmd } = useWebSocket()
+
+function runAlias (alias) {
+  cmd(alias)
+}
+
+function getAliases () {
+  return state.gameState.aliases.filter(a => !a.isDefault && !a.isSlot)
+}
+
+function getBottom () {
+  let bottomOffset = 40
+  if (state.options.showSideMovement) {
+    bottomOffset += 138
+  }
+
+  if (showHUD()) {
+    bottomOffset += 140
+  }
+
+  if (state.options.showQuickSlots) {
+    bottomOffset += 50
+  }
+
+  return bottomOffset + 'px'
+}
+
+</script>
+<style lang="less">
+.aliases {
+  position: absolute;
+  right: 5px;
+  z-index: 4;
+  max-width: 200px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  .n-button {
+    margin-right: 5px;
+    margin-bottom: 5px;
+  }
+}
+</style>
