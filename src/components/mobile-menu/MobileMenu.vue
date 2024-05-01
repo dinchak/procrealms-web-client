a1369100a95d35e145fd4606ae75f1b3b566b842
tom <dinchak@gmail.com>
2024-04-30 19:10:40 -0500

    cleanup

--- src/components/mobile-menu/MobileMenu.vue
+++ src/components/mobile-menu/MobileMenu.vue
@@ -6,12 +6,6 @@
         <div class="quick-slots">
           <QuickSlots :layout-mode="'mobile'"></QuickSlots>
         </div>
-        <div class="battle-area" v-show="state.gameState.battle.active">
-          <BattleControls></BattleControls>
-        </div>
-        <!-- <div class="map-actions">
-          <MapActions v-show="!state.gameState.battle.active"></MapActions>
-        </div> -->
         <div class="map-area" v-show="!state.gameState.battle.active">
           <MoveControls></MoveControls>
           <MiniMap v-if="state.options.showMobileMenuMap"></MiniMap>
@@ -30,9 +24,7 @@ import { NLayoutSider } from 'naive-ui'
 
 import MiniMap from '@/components/common/MiniMap.vue'
 import PlayerStats from '@/components/mobile-menu/PlayerStats.vue'
-import BattleControls from '@/components/mobile-menu/BattleControls.vue'
 import MoveControls from '@/components/mobile-menu/MoveControls.vue'
-import MapActions from '@/components/mobile-menu/MapActions.vue'
 import MiniStats from '@/components/mobile-menu/MiniStats.vue'
 import QuickSlots from '@/components/hud/QuickSlots.vue'
 
