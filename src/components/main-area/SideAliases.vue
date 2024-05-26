95c414831cf70add9e712563b238fc4bd44035be
ividyon <shrinkray@gmail.com>
2024-05-26 03:53:14 +0200

    paddings

--- src/components/main-area/SideAliases.vue
+++ src/components/main-area/SideAliases.vue
@@ -30,9 +30,18 @@ function getAliases () {
   flex-direction: row;
   flex-wrap: wrap;
   max-width: 200px;
-  max-height: 133px;
+  max-height: 130px;
   justify-content: center;
   align-content: flex-start;
+
+  @media screen and (max-width: 900px) {
+    .game.show-mobile-menu & {
+      max-width: 100px;
+    }
+  }
+  @media screen and (max-width: 600px) {
+    max-width: 100px;
+  }
 
   overflow-y: scroll;
   .n-button {
