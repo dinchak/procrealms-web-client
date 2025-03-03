c5c8c69258c542f8380ce849a457b745a7003e52
tom <dinchak@gmail.com>
2025-03-03 15:27:56 -0500

    updated battle view

--- src/components/battle/BattleStatus.vue
+++ src/components/battle/BattleStatus.vue
@@ -2,30 +2,31 @@
   <div class="battle-area">
     <div class="battle-status">
       <template v-for="side in ['good', 'vs', 'evil']" :key="side">
-        <div v-if="side === 'vs'" class="vs">VS</div>
+        <div v-if="side === 'vs'" class="vs-container">
+          <div class="vs">VS</div>
+        </div>
+
         <div v-if="side !== 'vs'" v-bind:class="getSideClass(side)">
-          <div v-for="row in getRows(side)" class="side-row" :key="row">
-            <div v-for="participant in getSideInRow(side, row)" class="entity" :key="participant.eid">
-              <TransitionGroup appear name="damage">
-                <div
-                    v-for="(anim, i) in state.animations.filter(a => a.eid == participant.eid && a.type == 'damage')"
-                    :key="anim.key"
-                    :style="{ left: `${10 + i * 50}px` }"
-                    :class="getAnimationClass(anim)"
-                >{{ anim.amount }}</div>
-              </TransitionGroup>
+          <div v-for="participant in getParticipants(side)" class="entity" :key="participant.eid">
+            <TransitionGroup appear name="damage">
+              <div
+                v-for="(anim, i) in state.animations.filter(a => a.eid == participant.eid && a.type == 'damage')"
+                :key="anim.key"
+                :style="{ left: `${10 + i * 50}px` }"
+                :class="getAnimationClass(anim)"
+              >{{ anim.amount }}</div>
+            </TransitionGroup>
 
-              <TransitionGroup appear name="healing">
-                <div
-                    v-for="(anim, i) in state.animations.filter(a => a.eid == participant.eid && a.type == 'healing')"
-                    :key="anim.key"
-                    :style="{ left: `${10 + i * 50}px` }"
-                    :class="getAnimationClass(anim)"
-                >{{ anim.amount }}</div>
-              </TransitionGroup>
+            <TransitionGroup appear name="healing">
+              <div
+                v-for="(anim, i) in state.animations.filter(a => a.eid == participant.eid && a.type == 'healing')"
+                :key="anim.key"
+                :style="{ left: `${10 + i * 50}px` }"
+                :class="getAnimationClass(anim)"
+              >{{ anim.amount }}</div>
+            </TransitionGroup>
 
-              <BattleEntity :entity="getPartyEntity(participant)" :participant="participant" :side="side"></BattleEntity>
-            </div>
+            <BattleEntity :entity="getPartyEntity(participant)" :participant="participant" :side="side"></BattleEntity>
           </div>
         </div>
       </template>
@@ -59,34 +60,6 @@ function getParticipants (side) {
   let participants = Object.values(state.gameState.battle.participants).filter(p => p.side == side)
   participants.sort((a, b) => { return a.sort < b.sort ? -1 : 1 })
   return participants
-}
-
-function getParticipantsPerRow () {
-  let m = window.matchMedia("(max-width: 1075px)")
-  return !m.matches ? 3 : 2 // 2 per row on mobile
-}
-
-// Gets the participants of a side which belong to a certain row.
-// Row begins at 1.
-function getSideInRow (side, row) {
-  const participants = getParticipants(side)
-  const perRow = getParticipantsPerRow()
-
-  const startIndex = (row * perRow) - perRow
-  const endIndex = (row * perRow)
-
-  return participants.slice(startIndex, Math.min(endIndex, participants.length)) // Clamp by amount of participants.
-}
-
-// Gets the amount of participant rows per side.
-// Ex. 7 participants = 3 rows (3, 3, 2).
-function getRows (side) {
-  const perRow = getParticipantsPerRow()
-
-  const len = getParticipants(side).length
-  const rows = Math.ceil(len / perRow)
-
-  return Math.max(1, rows) // To ward off against 3 participants returning 0 rows.
 }
 
 function getPartyEntity (participant) {
@@ -133,123 +106,117 @@ onBeforeUnmount(() => {
   justify-content: center;
   align-items: center;
 
-  .vs {
-    z-index: 1;
-    padding: 0;
-    margin: 0 4px;
-    flex: 0 0 0;
-    position: relative;
-    font-family: 'DOS', monospace;
-    animation: vs 0.4s ease-in forwards;
-    font-size: 1.1rem;
-    text-align: center;
-    letter-spacing: 2px;
-    //color: #c50f1f;
-    color: #f5f5f5;
-    //text-shadow:
-    //      0px -2px 2px black,
-    //      2px 0px 2px black,
-    //      -2px 0px 2px black,
-    //      0px -4px 4px #fff,
-    //      0px -4px 4px #fff,
-    //  0px -6px 6px #FF3,
-    //  0px -6px 6px #FF3,
-    //  0px -8px 8px #F90,
-    //  0px -8px 8px #F90,
-    //  0px -16px 12px #C33,
-    //  0px -16px 12px #C33;
-    text-shadow:
-          0px -2px 2px black,
-          2px 0px 2px black,
-          -2px 0px 2px black;
-
-    &::before, &::after {
-      animation: vsburn 0.6s ease-out 0.2s forwards;
-      opacity: 0;
-      z-index: 0;
-      position: absolute;
-      color: transparent;
-      bottom: 0;
-      left: 50%;
-      transform: translateX(-50%);
-      content: 'VS';
-      text-shadow: none;
-    }
-
-    &::before {
-      z-index: -1;
-      font-size: 1.25em;
+  .vs-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .vs {
+      z-index: 1;
+      margin: 0px 4px;
+      flex: 0 0 0;
+      position: relative;
+      font-family: 'DOS', monospace;
+      animation: vs 0.4s ease-in forwards;
+      font-size: 1.1rem;
+      text-align: center;
+      letter-spacing: 2px;
+      //color: #c50f1f;
+      color: #f5f5f5;
+      //text-shadow:
+      //      0px -2px 2px black,
+      //      2px 0px 2px black,
+      //      -2px 0px 2px black,
+      //      0px -4px 4px #fff,
+      //      0px -4px 4px #fff,
+      //  0px -6px 6px #FF3,
+      //  0px -6px 6px #FF3,
+      //  0px -8px 8px #F90,
+      //  0px -8px 8px #F90,
+      //  0px -16px 12px #C33,
+      //  0px -16px 12px #C33;
       text-shadow:
-            0 0 0 rgba(#ffb700, 0.7),
-            0 0 8px #F90;
-    }
-    &::after {
-      bottom: -2px;
-      z-index: -2;
-      font-size: 1.6em;
-      text-shadow:
-            0 0 0 rgba(#C33, 0.5),
-            0 0 8px #C33;
+            0px -2px 2px black,
+            2px 0px 2px black,
+            -2px 0px 2px black;
+
+      &::before, &::after {
+        animation: vsburn 0.6s ease-out 0.2s forwards;
+        opacity: 0;
+        z-index: 0;
+        position: absolute;
+        color: transparent;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        content: 'VS';
+        text-shadow: none;
+      }
+
+      &::before {
+        z-index: -1;
+        font-size: 1.25em;
+        text-shadow:
+              0 0 0 rgba(#ffb700, 0.7),
+              0 0 8px #F90;
+      }
+      &::after {
+        bottom: -2px;
+        z-index: -2;
+        font-size: 1.6em;
+        text-shadow:
+              0 0 0 rgba(#C33, 0.5),
+              0 0 8px #C33;
+      }
     }
   }
 
   .side {
     display: flex;
-    flex-direction: row-reverse;
-    flex-basis: 50%;
-
-    .side-row {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-
-      .entity {
-        align-self: stretch;
-        position: relative;
-        margin: 5px;
-
-        .damage {
-          position: absolute;
-          top: 0;
-          font-size: 1.4rem;
-          color: #ff3333;
-          padding: 5px 10px;
-          opacity: 0.8;
-          background-color: #101014;
-          z-index: 1;
-
-          &.crit {
-            line-height: 1.2rem;
-            font-size: 1.9rem;
-            color: #ffcc33;
-          }
-        }
-
-        .healing {
-          position: absolute;
-          top: -40px;
-          font-size: 1.4rem;
-          color: #33ff33;
-          padding: 5px 10px;
-          opacity: 0.8;
-          background-color: #101014;
-          z-index: 1;
-        }
-      }
-    }
+    flex-wrap: wrap;
+    margin: 12px 12px;
+    gap: 4px;
+    align-self: flex-start;
 
     &.good {
-      justify-content: end;
-      .side-row {
-        align-items: flex-start;
-      }
+      justify-content: flex-end;
     }
 
     &.evil {
       justify-content: start;
       flex-direction: row;
-      .side-row {
-        align-items: flex-start;
+    }
+
+    .entity {
+      align-self: stretch;
+      position: relative;
+      // margin: 5px;
+
+      .damage {
+        position: absolute;
+        top: 0;
+        font-size: 1.4rem;
+        color: #ff3333;
+        padding: 5px 10px;
+        opacity: 0.8;
+        background-color: #101014;
+        z-index: 1;
+
+        &.crit {
+          line-height: 1.2rem;
+          font-size: 1.9rem;
+          color: #ffcc33;
+        }
+      }
+
+      .healing {
+        position: absolute;
+        top: -40px;
+        font-size: 1.4rem;
+        color: #33ff33;
+        padding: 5px 10px;
+        opacity: 0.8;
+        background-color: #101014;
+        z-index: 1;
       }
     }
   }
@@ -328,41 +295,12 @@ onBeforeUnmount(() => {
   }
 }
 
-@media screen and (max-width: 1075px) {
-  .battle-status {
-    .game.show-mobile-menu & {
-      flex-direction: column;
-    }
-    .side {
-      flex-basis: auto;
-      flex-direction: column-reverse;
-      .side-row {
-        flex-direction: row;
-      }
-      &.evil {
-        flex-direction: column;
-        .side-row {
-          flex-direction: row;
-        }
-      }
-    }
-  }
-}
-
 @media screen and (max-width: 800px) {
   .battle-status {
     flex-direction: column;
     .side {
-      flex-direction: column-reverse;
-      .side-row {
-        flex-direction: column-reverse;
-      }
-      &.evil {
-        flex-direction: column;
-        .side-row {
-          flex-direction: column;
-        }
-      }
+      flex-direction: column;
+      align-self: center;
     }
   }
 }
